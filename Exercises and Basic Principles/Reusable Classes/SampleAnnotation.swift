f1f23213507f163a77016d4c8f42f0afead2fa77
Mark Hamilton <mark@mark-hamilton.com>
2016-02-21 06:34:16 -0800

    Create SampleAnnotation.swift

---
+++ Exercises and Basic Principles/Reusable Classes/SampleAnnotation.swift
@@ -0,0 +1,20 @@
+//
+//  SampleAnnotation.swift
+//  Sample class for adding annotations to MKMapView.
+//
+//  Created by Mark Hamilton on 2/21/16.
+//  Copyright © 2016 dryverless. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class SampleAnnotation: NSObject, MKAnnotation {
+    
+    var coordinate = CLLocationCoordinate2D()
+    
+    init(coordinate: CLLocationCoordinate2D) {
+        self.coordinate = coordinate
+    }
+    
+}
